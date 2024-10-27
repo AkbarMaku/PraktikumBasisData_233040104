0184bfbb8b542d148d41e20d121fe1f4faa487ac
AkbarMaku <aakbarmaku@gmail.com>
2024-10-27 16:07:20 +0700

    Mengerjakan tugas pertemuan 5

---
+++ latihan3/nomor4.sql
@@ -0,0 +1,6 @@
+INSERT INTO jurusan (nama_jurusan)
+VALUES ('informatika'),
+		('Pwk'),
+		('Mesin'),
+		('Pangan'),
+		('Industri');
---
+++ latihan3/nomor5.sql
@@ -0,0 +1,12 @@
+INSERT INTO Dosen_Wali (NIP, nama, tanggal_lahir, alamat)
+VALUES
+(233040101, 'Asep', '2004-05-12', 'Bandung'),
+(233040102, 'Isep', '2004-05-13', 'Bandung'),
+(233040103, 'Esep', '2004-05-14', 'Bandung'),
+(233040104, 'Osop', '2004-05-15', 'Bandung'),
+(233040105, 'Asep', '2004-05-16', 'Bandung'),
+(233040106, 'Tomat', '2004-05-17', 'Bandung'),
+(233040107, 'Jeruk', '2004-05-18', 'Bandung'),
+(233040108, 'Apel', '2004-05-19', 'Bandung'),
+(233040109, 'Ilham', '2004-05-20', 'Bandung'),
+(233040110, 'Ujang', '2004-05-21', 'Bandung')
